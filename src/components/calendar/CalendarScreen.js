8f1357c99afce2e265bfecf8d39b4b803cc9c9b2
Cristian Atencia <cristianatencia220673@correo.itm.edu.co>
2022-01-02 10:53:05 -0500

    configuración react-big-calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,55 @@
 import React from 'react'
 import Navbar from '../ui/Navbar'
+import moment from 'moment'
+import 'moment/locale/es'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { messages } from '../../helpers/calendar-messages-es';
+
+moment.locale('es');
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'cumpleaños del jefe',
+    start: moment().toDate(), 
+    end: moment().add(2, 'hours').toDate(), 
+    bgcolor: '#fafafa'
+}]
 
 export const CalendarScreen = () => {
+
+
+    const eventStyleGetter = (event , start, end , isSelected) => {
+
+       
+        const style = {
+            backgroundColor: '#367Cf7',
+            borderRadius: '0px',
+            opacity: 0.8 ,
+            display: 'block',
+            color: 'white'
+            
+        }
+
+        return {
+            style
+        }
+    };
+
     return (
-        <div>
+        <div className="calendar-screen">
            <Navbar />
+
+           <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages= {messages}
+                eventPropGetter={eventStyleGetter}
+            />
         </div>
     )
 }

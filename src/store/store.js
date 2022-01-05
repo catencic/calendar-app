324bf7f36ea8efa4a89b932d63e33ac1e00b1f46
Cristian Atencia <cristianatencia220673@correo.itm.edu.co>
2022-01-04 19:29:04 -0500

    configuración de redux y creación del store

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import thunk from 'redux-thunk';
+
+import { applyMiddleware, createStore,compose } from "redux";
+import { rootReducer } from "../reducers/rootReducer";
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+
+export const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+);
